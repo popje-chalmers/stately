28e0171dcd938ddc235ce2f2d577b454612fdfe4
Jeremy Pope <popje@chalmers.se>
2020-06-16 12:15:25 +0200

    Made it easier to change font sizes (StatelyFonts.java).

--- app/StatelyFonts.java
+++ app/StatelyFonts.java
@@ -4,13 +4,16 @@ import java.awt.*;
 
 public class StatelyFonts
 {
-    public Font title = new Font("Sans", Font.BOLD, 24);
-    public Font issue_cell = new Font("Sans", Font.PLAIN, 11);
-    public Font signal_cell = new Font("Sans", Font.PLAIN, 11);
-    public Font text_input = new Font("Sans", Font.PLAIN, 11);
-    public Font code = new Font("Courier", Font.PLAIN, 11);
-    public Font label = new Font("Sans", Font.PLAIN, 10); // Little Left Labels
-    public Font compile_info = new Font("Sans", Font.PLAIN, 10);
-    public Font top_bar = new Font("Sans", Font.PLAIN, 11);
+    public static final int LARGE = 24;
+    public static final int SMALL = 10;
+    public static final int REGULAR = 11;
+    public Font title = new Font("Sans", Font.BOLD, LARGE);
+    public Font issue_cell = new Font("Sans", Font.PLAIN, REGULAR);
+    public Font signal_cell = new Font("Sans", Font.PLAIN, REGULAR);
+    public Font text_input = new Font("Sans", Font.PLAIN, REGULAR);
+    public Font code = new Font("Courier", Font.PLAIN, REGULAR);
+    public Font label = new Font("Sans", Font.PLAIN, SMALL); // Little Left Labels
+    public Font compile_info = new Font("Sans", Font.PLAIN, SMALL);
+    public Font top_bar = new Font("Sans", Font.PLAIN, REGULAR);
     public Font checkbox = label;
 }
